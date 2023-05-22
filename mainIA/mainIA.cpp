2fab75214827dbe1b720c10f30f3bcbbe974f047
Naufal Dzakwan <naufal.dzakwan.ft19@mail.umy.ac.id>
2023-05-22 17:21:08 +0700

    mainIA

--- mainIA/mainIA.cpp
+++ mainIA/mainIA.cpp
@@ -7,6 +7,26 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	ibu* varIbu = new ibu("Linda");
+	ibu* varIbu2 = new ibu("Novi");
+	anak* varAnak1 = new anak("Naufal");
+	anak* varAnak2 = new anak("Adit");
+	anak* varAnak3 = new anak("Toni");
+
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varIbu2->tambahAnak(varAnak3);
+	varIbu2->tambahAnak(varAnak1);
+
+	varIbu->cetakAnak();
+	varIbu2->cetakAnak();
+	
+	delete varIbu;
+	delete varIbu2;
+	delete varAnak1;
+	delete varAnak2;
+	delete varAnak3;
+
+	return 0;
 }
 
