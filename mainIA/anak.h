c4441bffe63b993ec2aef480abfa2ff562f6b7be
Naufal Dzakwan <naufal.dzakwan.ft19@mail.umy.ac.id>
2023-05-22 17:21:33 +0700

    mainIA

--- mainIA/anak.h
+++ mainIA/anak.h
@@ -5,7 +5,7 @@ class anak {
 public:
 	string nama;
 	anak(string pNama) :nama(pNama) {
-		cout << "Anak \"" << nam << "\" ada\n";
+		cout << "Anak \"" << nama << "\" ada\n";
 	}
 	~anak() {
 		cout << "Anak \"" << nama << "\" tidak ada\n";
